c443b34af407cc683e40d8d4bb189a78932fefdd
alisson <alissonchaves2015@gmail.com>
2020-12-26 14:38:53 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function Home(){
+    return(
+        <div>           
+            <h2>Home 3</h2>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1);
+
+    function adicionaContador(){
+        setContador(contador + 1)
+    }
+    
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionaContador}>Adicionar</button> 
+        </div>
+    )
+}
+
+
+
+
+export default Home
